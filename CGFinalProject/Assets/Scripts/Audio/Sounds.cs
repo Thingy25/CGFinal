a1c9732060d1e532585f31dd0c4b4b2a01fb769b
Aquafeniz <54065380+Aquafeniz@users.noreply.github.com>
2021-04-19 17:29:56 -0500

    Sound
    Se agrego el Sonido en 3D Jajaj jejeje

---
+++ CGFinalProject/Assets/Scripts/Audio/Sounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sounds
+{
+    public string nombre;
+
+    public AudioClip clip;
+
+    public float volume;
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource fuente;
+}
