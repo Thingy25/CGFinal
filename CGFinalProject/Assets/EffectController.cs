51c4a48c81479103184e85b02a33fd420ca64f6b
danielg-r <d4nielg.r@gmail.com>
2021-04-19 18:53:03 -0500

    Sprint 1 Danielito

---
+++ CGFinalProject/Assets/EffectController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectController : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    [SerializeField] GameObject psObj1;
+    [SerializeField] GameObject psObj2;
+
+    [SerializeField] Gradient gradient;
+
+    GameObject activeEffectObject;
+
+    ParticleSystem ps;
+
+    float t;
+    float effectDuration;
+    bool isActive;
+    
+
+    void Start() {  
+        anim = null; //Placeholder      
+        ps = activeEffectObject?.GetComponent<ParticleSystem>();
+        effectDuration = ps.main.duration + ps.main.startLifetimeMultiplier;
+        t = effectDuration;
+    }
+
+    void ResetValues() {
+        ps = activeEffectObject?.GetComponent<ParticleSystem>();
+        effectDuration = ps.main.duration + ps.main.startLifetimeMultiplier;
+        t = effectDuration;
+    } 
+
+    public void SetActiveEffect(int effectIndex) {
+        switch (effectIndex)
+        {
+            case 0:
+            activeEffectObject = psObj1; break;
+            case 1:             
+            activeEffectObject = psObj2; break;
+        }
+        ResetValues();
+    }
+
+    void Update() {
+        if (t >= effectDuration) {
+            if (Input.GetButtonDown("Fire1")) {
+                anim?.SetBool("Animation", true);
+                activeEffectObject?.SetActive(true);
+                t = 0;
+            }
+            if (activeEffectObject.activeSelf && t > effectDuration) {
+                activeEffectObject?.SetActive(true);
+                anim?.SetBool("Animation", false);
+            }
+        }
+        t += Time.deltaTime;
+        
+    }
+}
---
+++ CGFinalProject/Assets/UIController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] Gradient gradient;
+
+}
