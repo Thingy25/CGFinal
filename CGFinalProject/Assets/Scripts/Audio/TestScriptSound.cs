77ec1b53ed1af5508452d37823d14ee1b563cd4f
Aquafeniz <54065380+Aquafeniz@users.noreply.github.com>
2021-05-02 13:49:25 -0500

    Cositas varias de Restrepo :)

--- CGFinalProject/Assets/Scripts/Audio/TestScriptSound.cs
+++ CGFinalProject/Assets/Scripts/Audio/TestScriptSound.cs
@@ -8,9 +8,12 @@ public class TestScriptSound : MonoBehaviour
     //public static Action<String> Sound;
     public ParticleSystem Particulas;
 
+    [SerializeField] float t2;
+
     [SerializeField] GameObject o1;
     [SerializeField] GameObject o2;
     [SerializeField] GameObject o3;
+
 
     private float t = 100;
     float state = 0;
@@ -25,16 +28,16 @@ public class TestScriptSound : MonoBehaviour
             state++;
         }
 
-        if(t > 15 && state == 1)
+        if(t > 0.9 && state == 1)
         {
             FindObjectOfType<AudioManager>().Play("Escudo Loop", o2);
             state++;
         }
 
-        if(t > 1600 && state == 2)
+        if(t > t2 && state == 2)
         {
             FindObjectOfType<AudioManager>().Stop("Escudo Loop", o2);
-            FindObjectOfType<AudioManager>().Play("Escudo FInal", o3);
+            FindObjectOfType<AudioManager>().Play("Escudo Final", o3);
             state++;
         }
         if (state != 0) t++;
