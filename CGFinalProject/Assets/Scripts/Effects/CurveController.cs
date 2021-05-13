b21a605edaabeb9c33122f8487441940fc30dd9e
Thingy25 <68366292+Thingy25@users.noreply.github.com>
2021-05-13 00:11:41 -0500

    Cambios prefab y escena Ramírez
    Script de control curva creado

---
+++ CGFinalProject/Assets/Scripts/Effects/CurveController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveController : MonoBehaviour
+{
+    [SerializeField] AnimationCurve curve;
+    [SerializeField] float effectDuration;
+    [SerializeField] float curveValue;
+
+    float lightIntensity;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
