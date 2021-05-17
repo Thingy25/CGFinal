d359678571e0b22198900c7df21f9bc27eb5ebab
Thingy25 <68366292+Thingy25@users.noreply.github.com>
2021-05-17 14:08:41 -0500

    Ajustes CurveController, sprite de flecha importado

--- CGFinalProject/Assets/Scripts/Effects/CurveController.cs
+++ CGFinalProject/Assets/Scripts/Effects/CurveController.cs
@@ -7,16 +7,23 @@ public class CurveController : MonoBehaviour
     [SerializeField] AnimationCurve curve;
     [SerializeField] float effectDuration;
     [SerializeField] float curveValue;
+    [SerializeField] float lightIntensity;
 
-    float lightIntensity;
+    [SerializeField] Light light;
+    [SerializeField] ParticleSystem ps;
+
+    ParticleSystem.SizeOverLifetimeModule psSize;
+
+    float t = 0;
     void Start()
     {
-        
+        psSize = ps.sizeOverLifetime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        curveValue = curve.Evaluate(t / effectDuration);
+        t += Time.deltaTime;
     }
 }
