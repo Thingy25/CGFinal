da24033306b86a9113b54b2448d85ac2fb3f0b50
Thingy25 <68366292+Thingy25@users.noreply.github.com>
2021-05-19 16:53:27 -0500

    Update CurveController.cs

--- CGFinalProject/Assets/Scripts/Effects/CurveController.cs
+++ CGFinalProject/Assets/Scripts/Effects/CurveController.cs
@@ -6,33 +6,42 @@ public class CurveController : MonoBehaviour
 {
     [SerializeField] AnimationCurve curve;
     [SerializeField] float effectDuration;
-    [SerializeField] float curveValue;
     [SerializeField] float lightIntensity;
     [SerializeField] float vol;
+    public float curveValue;
 
-    [SerializeField] Light light;
-    [SerializeField] ParticleSystem ps;
+    //Light light;
+    ParticleSystem ps;
 
     ParticleSystem.SizeOverLifetimeModule psSize;
+    ParticleSystem.LightsModule psLight;
 
     float t = 0;
     void Start()
     {
+        ps = GetComponent<ParticleSystem>();
         psSize = ps.sizeOverLifetime;
+        psLight = ps.lights;
     }
 
     // Update is called once per frame
     void Update()
     {
         curveValue = curve.Evaluate(t / effectDuration);
-        light.intensity = lightIntensity * curveValue;
+        //light.intensity = lightIntensity * curveValue;
         //Llamar audio.volume = vol * curveValue;
         psSize.size = curveValue;
+        psLight.rangeMultiplier = curveValue;
         t += Time.deltaTime;
     }
 
    public void CurveEvaluation()
    {
-
-   }
+        curveValue = curve.Evaluate(t / effectDuration);
+        //light.intensity = lightIntensity * curveValue;
+        //Llamar audio.volume = vol * curveValue;
+        psSize.size = curveValue;
+        psLight.rangeMultiplier = curveValue;
+        t += Time.deltaTime;
+    }
 }
