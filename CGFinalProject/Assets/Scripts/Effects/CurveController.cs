c1e64209b6071224d10886a7d1440fea485367d5
Thingy25 <68366292+Thingy25@users.noreply.github.com>
2021-05-21 17:27:46 -0500

    Update CurveController.cs

--- CGFinalProject/Assets/Scripts/Effects/CurveController.cs
+++ CGFinalProject/Assets/Scripts/Effects/CurveController.cs
@@ -8,14 +8,14 @@ public class CurveController : MonoBehaviour
     static CurveController instance;
 
     [SerializeField] AnimationCurve curve1;
-    [SerializeField] AnimationCurve curve2;
+    //[SerializeField] AnimationCurve curve2;
     [SerializeField] float effectDuration;
     [SerializeField] float lightIntensity;
     [SerializeField] float vol;
     public float curve1Value;
-    public float curve2Value;
+    //public float curve2Value;
 
-    public EffectTypes effectType;
+    //public EffectTypes effectType;
 
     //Light light;
     ParticleSystem ps;
@@ -27,10 +27,6 @@ public class CurveController : MonoBehaviour
     ParticleSystem.SizeOverLifetimeModule ps1Size;
     ParticleSystem.LightsModule psLight;
 
-    ParticleSystem.SizeOverLifetimeModule ps2Size;
-
-    ParticleSystem.SizeOverLifetimeModule ps3Size;
-
     float t = 0;
     void Awake()
     {
@@ -40,29 +36,18 @@ public class CurveController : MonoBehaviour
         //psLight = ps.lights;
         ps1Size = ps1.sizeOverLifetime;
         psLight = ps1.lights;
-        ps2Size = ps2.sizeOverLifetime;
-        ps3Size = ps3.sizeOverLifetime;
+        //ps2Size = ps2.sizeOverLifetime;
+        //ps3Size = ps3.sizeOverLifetime;
     }
 
     void Update()
     {
-        switch (effectType)
-        {
-            //case effectType.Shield:
-            //    {
-
-            //    }
-            //case effectType.Arrow:
-            //    {
-
-            //    }
-        }
         if(t>=effectDuration)
         {
             t = 0;
         }
         curve1Value = curve1.Evaluate(t / effectDuration);
-        curve2Value = curve2.Evaluate(t / effectDuration);
+        //curve2Value = curve2.Evaluate(t / effectDuration);
         //light.intensity = lightIntensity * curveValue;
         //Llamar audio.volume = vol * curveValue;
         ps1Size.size = curve1Value;
