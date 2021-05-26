37b430deced70c434099e3231ded0732f64a790e
danielg-r <d4nielg.r@gmail.com>
2021-05-25 20:27:19 -0500

    Dotween + Anticipación flecha

---
+++ CGFinalProject/Assets/DTSetup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DTSetup : MonoBehaviour
+{
+    [SerializeField] DOTweenAnimation[] tweens;
+    float[] initialDurations;
+    
+    private void OnEnable() {
+        foreach (DOTweenAnimation tw in tweens) {
+            tw.duration = EffectController.Instance.SpeedMultiplier;
+            tw.DOPlay();
+        }
+        GetComponent<Projector>().material.SetColor("_Color", ShadersController.Instance.SelectedColor);
+
+    }
+
+    public void RestartTweens() {
+        foreach (DOTweenAnimation tw in tweens) {
+            tw.DORestart();
+        }
+    }
+    
+
+
+}
